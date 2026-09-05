3f8e039bf1324764322d3fd414e99237566d68d5
Jose Manuel Cornejo <jose.cornejo.lupa@gmail.com>
2026-09-05 18:38:50 -0500

    Initial skeleton for mod_colabnotebook plugin

---
+++ db/upgrade.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Upgrade steps for mod_colabnotebook.
+ *
+ * @package    mod_colabnotebook
+ * @copyright  2026 José Cornejo <jose.cornejo.lupa@gmail.com>
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Custom upgrade steps for mod_colabnotebook.
+ *
+ * @param int $oldversion the version we are upgrading from.
+ * @return bool always true.
+ */
+function xmldb_colabnotebook_upgrade($oldversion) {
+    global $DB;
+
+    // No upgrade steps yet.
+
+    return true;
+}
